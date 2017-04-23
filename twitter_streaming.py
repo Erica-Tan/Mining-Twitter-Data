98aa628b7512237746b6c0a106dddf26a32d1768
yao <541198523@qq.com>
2017-04-23 18:48:46 +1200

    update

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -9,46 +9,69 @@ import settings
 total_count = 1
 file_name = str(time.time())
 
+# format json
+with open(os.path.join(settings.DATA_DIR, 'twitter'+file_name+'.json'), 'a') as f:
+        f.write('[')
+
+
 class MyStreamListener (StreamListener):
-        def __init__(self, limit=10000):
-            self.limit = limit
+	def __init__(self, limit=30000):
+		self.limit = limit
 
-        def on_data(self, raw_data):
-            try:
-                if 'delete' in raw_data:
-                    return True
+	def on_data(self, raw_data):
+		global total_count
+		global file_name
+					 
+		try:
+			#ignore delete record
+			if 'delete' in raw_data:
+				return True
 
-                global total_count
-                global file_name
-                
-                if divmod(total_count, self.limit)[1] == 0:
-                    file_name = str(time.time())
 
-                total_count += 1
+			# format json
+			if divmod(total_count, self.limit)[1] == 0:
+				with open(os.path.join(settings.DATA_DIR, 'twitter'+file_name+'.json'), 'a') as f:
+					f.write(']')
 
-                #print (raw_data)
+				file_name = str(time.time())
 
-                with open(os.path.join(settings.DATA_DIR, 'twitter'+file_name+'.json'), 'a') as f:
-                    f.write(raw_data)
+				with open(os.path.join(settings.DATA_DIR, 'twitter'+file_name+'.json'), 'a') as f:
+					f.write('[')
+			
+			#increase total_count
+			total_count += 1
 
-                return True
+			#if it is the last record, no comma at the end
+			if_last_record = divmod(total_count, self.limit)[1] == 0
 
-            except BaseException as e:
-                #print ('failed ondata', str(e))
-                time.sleep(5)
-                return True
+			#print (raw_data)
 
-        def on_error (self, status) :
-            #returning False in on_data disconnects the stream
-            if status == 420:
-                return False
+			with open(os.path.join(settings.DATA_DIR, 'twitter'+file_name+'.json'), 'a') as f:
+				f.write(raw_data)
+				if if_last_record == False:
+					f.write(',')
+
+			return True
+
+		except BaseException as e:
+			#print ('failed ondata', str(e))
+			time.sleep(5)
+			return True
+
+	def on_error (self, status) :
+		#returning False in on_data disconnects the stream
+		if status == 420:
+			return False
+
+
+
 
 auth = OAuthHandler(settings.CUSTOMER_KEY, settings.CUSTOMER_SECRET)
 auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_SECRET)
 
 while True:
     try:
-        twitterStream = Stream(auth, MyStreamListener())
+        twitterStream = Stream(auth, MyStreamListener(10))
         twitterStream.sample()
     except KeyboardInterrupt:
         # Or however you want to exit this loop
